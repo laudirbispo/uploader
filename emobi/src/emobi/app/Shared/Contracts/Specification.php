26bc2faf36646358ca901e80958a6b975dc0dafa
laudirbispo <laudirbispo@pitlook.com>
2019-01-29 10:35:54 -0200

    Revert "Revert "Revert "Starting"""
    This reverts commit 74944bffbbca944db46a6802292db85823ca2177.

--- emobi/src/emobi/app/Shared/Contracts/Specification.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-namespace app\Shared\Contracts;
-
-interface Specification 
-{
-	public function isSatisfiedBy($object) : bool ;
-}
--- emobi/src/emobi/libs/Encryption/EncryptException.php
+++
@@ -1,4 +0,0 @@
-<?php declare(strict_types=1);
-namespace libs\Encryption;
-
-class EncryptException extends \Exception {}
